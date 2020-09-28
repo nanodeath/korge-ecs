5057da7c4a517e224a931c02d3ecc7b03984d7f5
nanodeath <max@maxaller.name>
2020-09-27 21:58:02 -0700

    Adding more KDoc, removing unused ComponentMapper#destroy.

--- korge-ecs/src/commonMain/kotlin/org/korge/ecs/ComponentMapper.kt
+++ korge-ecs/src/commonMain/kotlin/org/korge/ecs/ComponentMapper.kt
@@ -16,9 +16,29 @@ class ComponentMapper<T : Component> internal constructor(private val world: Wor
     internal val addComponents = ArrayList<T>()
     internal val removeEntities = IntArrayList()
 
+    /**
+     * Retrieve the component data of type [T] associated with [entity].
+     *
+     * @throws IllegalArgumentException if the entity doesn't have a component of this type attached.
+     * @return a valid component
+     */
     operator fun get(entity: Int): T = components[entity] ?: throw IllegalArgumentException("Entity $entity does not have a $componentType")
+
+    /**
+     * Retrieves the component data of type [T] associated with [entity] if [entity] has a component of this type.
+     *
+     * @return a valid component, or null if the entity didn't have a component of this type.
+     */
     fun tryGet(entity: Int): T? = components[entity]
 
+    /**
+     * Adds the provided [component] data to the given [entity].
+     *
+     * If components are added mid-World [tick][World.tick], changes aren't applied until the end of the tick.
+     * Otherwise the component is added immediately.
+     *
+     * If a component of type [T] already exists on [entity], the existing component will be overwritten.
+     */
     fun addComponent(entity: Int, component: T) {
         if (world.processing) {
             addEntities.add(entity)
@@ -36,6 +56,14 @@ class ComponentMapper<T : Component> internal constructor(private val world: Wor
         }
     }
 
+    /**
+     * Removes component data for the given [entity].
+     *
+     * If components are removed mid-World [tick][World.tick], changes aren't applied until the end of the tick.
+     * Otherwise the component is removed immediately.
+     *
+     * If no component of type [T] exists on [entity], this method does nothing.
+     */
     fun removeComponent(entity: Int) {
         if (world.processing) {
             removeEntities.add(entity)
@@ -52,11 +80,14 @@ class ComponentMapper<T : Component> internal constructor(private val world: Wor
         }
     }
 
+    /**
+     * Checks whether at this moment, [entity] has component data of type [T].
+     *
+     * Note that this does **not** check for whether a queued add/remove operation would affect the return value.
+     *
+     * @return true if this [ComponentMapper] currently has data that can be retrieved using [get].
+     */
     fun hasEntity(entity: Int): Boolean = entity in components
-
-    fun destroy(entity: Int) {
-        destroy(entity, notifyWorld = true)
-    }
 
     internal fun destroy(entity: Int, notifyWorld: Boolean) {
         if (components.remove(entity) != null) {
